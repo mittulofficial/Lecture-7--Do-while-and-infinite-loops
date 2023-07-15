44126517bbec0c471668ce36703add9505e7568a
mittulofficial <111638490+mittulofficial@users.noreply.github.com>
2023-07-16 01:01:23 +0530

    Create breakinginfiniteloop.cpp
    this is about breaking infinite loop using while loop

---
+++ breakinginfiniteloop.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//
+//  Created by MITTUL KUMAR
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{int password,c=0;
+    cout<<"enter password";
+    cin>>password;
+    while(1)//the condition in while loop,until it is true,the loop will run
+    {
+        cout<<"chocolates";
+        c++;
+        if(c>100)
+            break;
+    }
+    return 0;
+}
