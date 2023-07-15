7aa724c772a558729f5d484f43725183da1392de
mittulofficial <111638490+mittulofficial@users.noreply.github.com>
2023-07-16 00:58:06 +0530

    Create infiniteloop.cpp
    this is while infinite loop

---
+++ infiniteloop.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//
+//  Created by MITTUL KUMAR
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{int password;
+    cout<<"enter password";
+    cin>>password;
+    while(1)//the condition in while loop,until it is true,the loop will run
+    {
+        cout<<"chocolates";
+    }
+    return 0;
+}
