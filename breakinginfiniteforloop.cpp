38bdf53d7b928302620b73d46a5b620f826b6639
mittulofficial <111638490+mittulofficial@users.noreply.github.com>
2023-07-16 01:04:11 +0530

    Create breakinginfiniteforloop.cpp
    the is about breaking for loop using for loop

---
+++ breakinginfiniteforloop.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//
+//  Created by MITTUL KUMAR
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{int i;
+    for(i=0; ;i++)
+        
+    {
+        cout<<i;
+        if(i>100)break;
+    }
+    return 0;
+}
