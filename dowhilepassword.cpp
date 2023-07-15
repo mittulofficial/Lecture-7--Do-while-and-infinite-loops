fa3ca2d109754af7b6aa06ceba372370416bf235
mittulofficial <111638490+mittulofficial@users.noreply.github.com>
2023-07-16 00:54:27 +0530

    Create dowhilepassword.cpp
    this is about taking password using do while

---
+++ dowhilepassword.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//
+//  Created by MITTUL KUMAR
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{int password;
+    cout<<"enter password";
+    cin>>password;
+    do{
+        cin>>password;
+    }
+    while(password<999999);
+   // {
+  //      cout<<"the password does not meet required condition please enter correct password again"<<endl;
+  //      cin>>password;
+        
+  //  }
+  //  cout<<"the password emtered is v"
+    return 0;
+}
