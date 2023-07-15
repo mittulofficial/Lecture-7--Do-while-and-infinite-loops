afac018d9a6a498883298f9e27694a78935bab8f
mittulofficial <111638490+mittulofficial@users.noreply.github.com>
2023-07-16 01:09:10 +0530

    Create breakingdowhileinfiniteloop.cpp
    this will print only 100 times in do while loop

---
+++ breakingdowhileinfiniteloop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{ int c=0;
+    do
+    {
+        cout << "chocolates" << endl;
+        c++;
+        if(c>100)break;
+    } while (true);
+
+    return 0;
+}
